b7e3d189eee6ea7b012aacef2614304f7f69f689
mhayes787 <maurice.d.hayes@gmail.com>
2020-03-22 23:19:48 -0400

    Created routing for Home and About components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//Components
+import Home from './Components/Home/home';
+import About from './Components/About/about';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+
+
+      </Router>
     </div>
   );
 }

23571445f8fe977f6aa0ba7144a68777bad36def
mhayes787 <maurice.d.hayes@gmail.com>
2020-03-23 00:01:24 -0400

    added header to be viewed from every page and corrected link feature in header component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 //Components
 import Home from './Components/Home/home';
 import About from './Components/About/about';
+import Header from './Components/Header/header';
 
 
 
 function App() {
   return (
     <div className="App">
+      <div><Header /></div>
       <Router>
 
         <Route exact path="/" component={Home} />
--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Link } from 'react-router-dom';
+
 
 
 class Header extends Component {

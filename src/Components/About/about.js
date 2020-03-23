2ec84de2b63397c9fa75f4390d2282a965d2de37
mhayes787 <maurice.d.hayes@gmail.com>
2020-03-22 23:02:53 -0400

    created Home and About components

---
+++ src/Components/About/about.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                About Us Page!!
+            </div>
+        )
+    }
+}
+
+export default About

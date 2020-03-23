3b8306bf8a6d185efff11a1633e0e3c3eba35979
mhayes787 <maurice.d.hayes@gmail.com>
2020-03-22 23:34:12 -0400

    added home and about link routers to header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
+            <div className="Five-header">
+                <ul className="menu-ul">
+                    <li><Link to="/" className="Header-link" style={{ textDecoration: 'none' }}>HOME</Link></li>
+                    <li><Link to="/About" className="Header-link" style={{ textDecoration: 'none' }}>ABOUT</Link></li>
+
+                </ul>
+
             </div>
-        )
+        );
     }
 }
 

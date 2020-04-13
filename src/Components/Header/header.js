9d8b8e45de69015815ccf8b8fd1bb3e4fcc07d65
mhayes787 <maurice.d.hayes@gmail.com>
2020-04-13 09:19:52 -0400

    Header styling

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Link } from 'react-router-dom';
+import './header.scss';
+
+//Design for this component only
+// import './header.scss';
+
 
 
 
 class Header extends Component {
     render() {
         return (
-            <div className="Five-header">
+            <div className="mainHeader">
+                <h1 id="WelcomePage"> Welcome To Maurice Hayes Photography</h1>
                 <ul className="menu-ul">
                     <li><Link to="/" className="Header-link" style={{ textDecoration: 'none' }}>HOME</Link></li>
                     <li><Link to="/About" className="Header-link" style={{ textDecoration: 'none' }}>ABOUT</Link></li>
